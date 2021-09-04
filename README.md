971801a09591070715e5071d9a2b0bed938612de
ryo-i <iidazz0109@gmail.com>
2021-09-04 09:56:01 +0900

    リズムを配列に直書きするテスト #5

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## 進捗
 
-- リズムを少数第二位で切り捨て [#5](https://github.com/ryo-i/beat-player/issues/5)
+- リズムを配列に直書きするテスト [#5](https://github.com/ryo-i/beat-player/issues/5)
 - CSS修正（背景色、余白など） [#6](https://github.com/ryo-i/beat-player/issues/6)
 
 ## ブログ
--- components/Inner.tsx
+++ components/Inner.tsx
@@ -179,10 +179,37 @@ function Inner() {
 
   // ビート再生設定
   const playBeat = (kickRtm, snareRtm, hihatRtm) => {
+    const kickRtmTest = [
+      "0:0.02:0",
+      "0:2.02:0"
+    ];
+    const snareRtmTest = [
+      "0:1.02:0",
+      "0:3.02:0"
+    ];
+    const hihatRtmTest = [
+      "0:0:0",
+      "0:0.25:0",
+      "0:0.5:0",
+      "0:0.75:0",
+      "0:1:0",
+      "0:1.25:0",
+      "0:1.5:0",
+      "0:1.75:0",
+      "0:2:0",
+      "0:2.25:0",
+      "0:2.5:0",
+      "0:2.75:0",
+      "0:3:0",
+      "0:3.25:0",
+      "0:3.5:0",
+      "0:3.75:0"
+    ];
+
       setSynth();
-      let kickPart = new Tone.Part(kickSynth, kickRtm).start();
-      let snarePart = new Tone.Part(snareSynth, snareRtm).start()
-      let hihatPart = new Tone.Part(hihatSynth, hihatRtm).start();
+      let kickPart = new Tone.Part(kickSynth, kickRtmTest).start();
+      let snarePart = new Tone.Part(snareSynth, snareRtmTest).start()
+      let hihatPart = new Tone.Part(hihatSynth, hihatRtmTest).start();
       kickPart.loop = true;
       snarePart.loop = true;
       hihatPart.loop = true;
